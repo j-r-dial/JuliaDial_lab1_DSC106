cb10ccb99728a01b0ca77a079afe3deee3f43a70
j-r-dial <jrdial@ucsd.edu>
2026-04-24 16:41:15 -0700

    Update global.js

--- global.js
+++ global.js
@@ -112,3 +112,42 @@ select.addEventListener('input', function (event) {
   
     location.href = url;
   });
+
+  export async function fetchJSON(url) {
+    try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      console.log(response)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+  export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+    for (let i=0; i < project.length; i+=1){
+      const current = project[i]
+      const article = document.createElement('article');
+      article.innerHTML = `
+      <${headingLevel}>${current.title}</${headingLevel}>
+      <img src="${current.image}" alt="${current.title}">
+      <p>${current.description}</p>
+  `;
+      containerElement.appendChild(article);
+    
+    }
+   
+  }
+
+  export async function fetchGitHubData(username) {
+    
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
+
+  
