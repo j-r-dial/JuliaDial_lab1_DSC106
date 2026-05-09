1ecae04c835fb3421540b31e9d5ea79242457c3d
j-r-dial <jrdial@ucsd.edu>
2026-05-08 19:23:24 -0700

    Update global.js

--- global.js
+++ global.js
@@ -22,6 +22,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'Resume/', title: 'Resume' },
+    {url: 'meta/', title: 'Meta'},
     {url: 'https://github.com/j-r-dial', title: "Profile"}
 
 ];
